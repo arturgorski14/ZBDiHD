15c3ccfc3c9625b02439782d82b46a7db8107811
ArturGorski14 <arturgorski14@gmail.com>
2020-06-14 09:56:54 +0200

    added users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+# registration
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST or None)
+
+        if form.is_valid():
+            user = form.save()
+
+            # get the raw password
+            raw_password = form.cleaned_data.get('password1')
+
+            # authenticate the user
+            user = authenticate(username=user.username, password=raw_password)
+
+            # login the user
+            login(request, user)
+
+            return redirect('main:home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
